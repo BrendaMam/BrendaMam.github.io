2d5566fc0c95ba8bce0306a9abf188998cf22319
karlamip <95324281+karlamip@users.noreply.github.com>
2022-02-11 22:02:53 -0600

    script estado

--- Scripts/estado.js
+++ Scripts/estado.js
@@ -1,23 +1,11 @@
-/*function validar(){
-    var estado = ""
-    var estado = document.getElementById("estado").value
-
-    mixpanel.track(estado)
-    
-    if(estado){
-        console.log(estado)
-        
-    }
-}*/
-
 function estado(){
     var estado = document.getElementById("estado").value
     
     if(estado == ""){
-        console.log("No se recibió información del estado")
+        //console.log("No se recibió información del estado")
         mixpanel.track("empty-state")
     }else{
-        console.log(estado)
+        //console.log(estado)
         mixpanel.track('Estado registrado', {'Estado': estado});
         
     }
