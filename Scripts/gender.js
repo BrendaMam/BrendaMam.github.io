45769ef9a9a253429f093580096f1cc5cd8aa246
mjmartin98 <95326159+mjmartin98@users.noreply.github.com>
2022-02-14 18:22:04 -0600

    Gender

---
+++ Scripts/gender.js
@@ -0,0 +1,17 @@
+
+function gender () {
+    var fema= document.getElementById('Mujer')
+    var male= document.getElementById('Hombre')
+    
+
+    if(sexo == "fema") {
+        mixpanel.track({'Gender': 'Female'});
+       
+    }  else {
+            mixpanel.track({'Gender': 'Male'});
+            
+        }
+   
+    }
+
+
