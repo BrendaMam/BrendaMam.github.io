be502b6acc8e4283ac47708500c4bd6e5cdcaa00
mjmartin98 <95326159+mjmartin98@users.noreply.github.com>
2022-02-15 19:48:00 -0600

    Update gender.js

--- Scripts/gender.js
+++ Scripts/gender.js
@@ -1,11 +1,17 @@
-
 function gender() {
     var fema= document.getElementById('Mujer')
     var male= document.getElementById('Hombre')
     
+    console.log(fema+male)
 
-    if(sexo == "fema") {
-        mixpanel.track({'Gender': 'Female'});
+    var sexo= document.querySelector("input[name='Sexo']:checked").value
+
+    console.log(sexo)
+
+
+    if(sexo == "Mujer") {
+        mixpanel.track({'Gender': 'Female'}); 
+        console.log("Selecciona Mujer")
        
     }  else {
             mixpanel.track({'Gender': 'Male'});
@@ -13,5 +19,4 @@ function gender() {
         }
    
     }
-
 
